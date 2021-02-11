66f01ddc0e7688cb6fb019ff28cc6bb13eca1c1d
Daniel Svitan <dankosaurus9@gmail.com>
2021-02-11 18:52:33 +0100

    adds tests

--- src/pm.py
+++ src/pm.py
@@ -5,9 +5,10 @@ from commands.add_modules import *
 args = sys.argv
 args.pop(0)
 
-build_path = args[0]
+command = args[0]
+path = args[1]
 
 
 if __name__ == "__main__":
-    if "add-modules" in args:
-        add_modules(build_path)
+    if "add-modules" == command:
+        add_modules(path)
---
+++ src/tests/pm_tests.py
@@ -0,0 +1,13 @@
+import unittest
+import subprocess
+
+
+class MyTestCase(unittest.TestCase):
+    def test_pm(self):
+        self.assertEqual(
+            subprocess.check_output("C:\\Users\\danko\\AppData\\Local\\Programs\\Python\\Python39\\python.exe D:/Desktop/Coding/Python/PyManager/src/pm.py add-modules D:\\Desktop\\Coding\\Python\\test"),
+            "Cloning module tkinter...\r\nModule tkinter cloned\r\n".encode())
+
+
+if __name__ == '__main__':
+    unittest.main()
